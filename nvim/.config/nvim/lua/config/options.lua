cab2414ce49e663154d5242fbe23df10498db6dc
Carlos Moreira <carlosmoreira.bji@gmail.com>
2025-04-12 18:47:21 -0300

    feat: add LazyVim configuration

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,20 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+
+-- options.lua
+
+vim.opt.wrap = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.opt.cursorcolumn = true
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.timeoutlen = 300
